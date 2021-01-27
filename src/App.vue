2ccbd5bbc4b76bcea63926c8e0832171c684f7af
Ivan <fangyin_chen@126.com>
2021-01-27 18:01:56 +0800

    typescript import module check error

--- src/App.vue
+++ src/App.vue
@@ -15,21 +15,21 @@ const currentuser: UserProps = {
 };
 const testData: ColumnProps[] = [
   {
-    id: 1,
+    id: '1',
     title: "test1的专栏",
     description:
       "这是的test1专栏，有一段非常有意思的简介，可以更新一下欧, 这是的test1专栏，有一段非常有意思的简介，可以更新一下欧",
     // avatar: "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_150,w_150",
   },
   {
-    id: 2,
+    id: '2',
     title: "test2的专栏",
     description: "这是的test2专栏，有一段非常有意思的简介，可以更新一下欧",
     avatar:
       "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100",
   },
   {
-    id: 3,
+    id: '3',
     title: "test3的专栏",
     description:
       "这是的test1专栏，有一段非常有意思的简介，可以更新一下欧 这是的test1专栏，有一段非常有意思的简介，可以更新一下欧",
@@ -37,7 +37,7 @@ const testData: ColumnProps[] = [
       "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100",
   },
   {
-    id: 4,
+    id: '4',
     title: "test4的专栏",
     description: "这是的test2专栏，有一段非常有意思的简介，可以更新一下欧",
     avatar:
